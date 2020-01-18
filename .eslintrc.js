b0ba0cb8530e97d00a14851bd999a4c8f39929a9
Yoshihide Jimbo <yjimbo@gmail.com>
2020-01-18 15:44:21 +0900

    Update eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['prettier', '@typescript-eslint'],
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
 };
